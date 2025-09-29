a6ee454ef0f2e795dfbdd82a0962573f7f622514
vvalerya <1997valerya1997@gmail.com>
2025-09-29 13:09:16 +0300

    Add new Entities

---
+++ ConsoleApp8/Word.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp8
+{
+        internal class Word
+        {
+                public string EnglishText { get; set; }
+                public string Translation { get; set; }
+                public string ExampleSentence { get; set; }
+
+                public override string ToString()
+                {
+                        return EnglishText + "\t" + Translation + "\t" + ExampleSentence;
+                }
+        }
+}
